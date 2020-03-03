9923b6791c0e33726dc46f7bda378840c6963335
jerneypompe <61182331+jerneypompe@users.noreply.github.com>
2020-03-03 15:11:49 +0100

    update3

--- charts.js
+++ charts.js
@@ -1,3 +1,4 @@
+// Code van dataChart1
 var ctx = document.getElementById('dataChart1');
 var dataChart1 = new Chart(ctx, {
   type: 'line',
@@ -21,7 +22,7 @@ var dataChart1 = new Chart(ctx, {
       },
       {
         label: "Temperature inside °C", //title chart
-        data: [22, 20, 21, 20, 19, 19, 18, 19.5, 21, 22, 22.5, 21, 22, 22, 21.5, 21, 21, 20, 20, 19, 20, 19.5, 20, 19, 18],// the actual data binnentemperatuur
+        data: [22, 20, 21, 20, 20.1, 19, 20, 19.5, 21, 22, 22.5, 21, 22, 22, 21.5, 21, 21, 20, 20, 19, 20, 19.5, 20, 19, 19.5],// the actual data binnentemperatuur
         fill: 'orgin',
         backgroundColor: 'blue',
         borderColor: 'blue',
@@ -56,4 +57,60 @@ var dataChart1 = new Chart(ctx, {
     }]
   }
 }
-})
+});
+// Code van dataChart2
+var ctx = document.getElementById('dataChart2');
+var dataChart2 = new Chart(ctx, {
+  type: 'bar', //horizontalBar grotere schermen
+  data: {
+    labels: ['Exercise', 'Looking at View', 'Sleep', 'Eat', 'Watch TV', 'Contact Family'],
+    datasets: [
+    {
+      label: "Hours per day",
+      borderColor: ['#8e44ad', '#3498db', '#1abc9c', '#f1c40f', '#e67e22', '#e74c3c'],
+      backgroundColor: [
+        'rgba(142, 68, 173, 0.2)', 
+        'rgba(52, 152, 219, 0.2)', 
+        'rgba(26, 188, 156, 0.2)', 
+        'rgba(241, 196, 15, 0.2)', 
+        'rgba(230, 126, 34, 0.2)', 
+        'rgba(231, 76, 60, 0.2)'
+        ],
+      borderWidth: 1,
+      data: [2, 4, 8, 3.2, 1.5, 0.5, 2],
+    }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Average Activities',
+      fontSize: 18,
+      position: 'top',
+    },
+    animation: {
+      duration: 2000,
+      easing: 'easeInCubic',
+    },
+    scales: {
+      yAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Hours',
+          fontStyle: 'bold',
+          fontColor: 'black',
+        }
+      }],
+      xAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Activities',
+          fontStyle: 'bold',
+          fontColor: 'black',
+        }
+    }]
+    }
+  }
+});
